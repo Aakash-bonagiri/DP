1d2ee107e3a7e48491f4d55d12c9f4a9739a16d5
Aakash-bonagiri <101444061+Aakash-bonagiri@users.noreply.github.com>
2025-07-27 10:17:08 +0530

    Update abstract-factory.py

--- abstract-factory.py
+++ abstract-factory.py
@@ -1,29 +1,39 @@
+import random
+
+class Course_At_GFG:
+    def __init__(self, courses_factory=None):
+        self.course_factory = courses_factory
+
+    def show_course(self):
+        course = self.course_factory()
+        print(f'We have a course named {course}')
+        print(f'its price is {course.Fee()}')
+
 class DSA:
-    def price(self):
+    def Fee(self):
         return 11000
 
     def __str__(self):
         return "DSA"
 
 class STL:
-    def price(self):
+    def Fee(self):
         return 8000
 
     def __str__(self):
         return "STL"
 
 class SDE:
-    def price(self):
+    def Fee(self):
         return 15000
 
     def __str__(self):
         return 'SDE'
 
-if __name__ == "__main__":
-    sde = SDE()
-    dsa = DSA()
-    stl = STL()
+def random_course():
+    return random.choice([SDE, STL, DSA])()
 
-    print(f'Name of the course is {sde} and its price is {sde.price()}')
-    print(f'Name of the course is {dsa} and its price is {dsa.price()}')
-    print(f'Name of the course is {stl} and its price is {stl.price()}')
+if __name__ == "__main__":
+    course = Course_At_GFG(random_course)
+    for i in range(5):
+        course.show_course()
